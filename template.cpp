ff176ffd0563056339e859471cee7b1bfd1d3cf5
Austin <39919091+Austincardoza@users.noreply.github.com>
2021-06-03 22:59:43 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,8 +1,8 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 #define gc getchar_unlocked
-#define fo(i, n) for (int i = 0; i < n; i++)
-#define Fo(i, k, n) for (int i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define fo(i,n) for(int i=0;i<n;i++)
+#define Fo(i,k,n) for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
 #define ll long long
 #define pb push_back
 #define mp make_pair
@@ -11,7 +11,7 @@ using namespace std;
 #define all(x) x.begin(), x.end()
 #define clr(x) memset(x, 0, sizeof(x))
 #define sortall(x) sort(all(x))
-#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
 #define PI 3.1415926535897932384626
 #define mod 1000000007
 typedef pair<int, int> pii;
@@ -23,18 +23,20 @@ typedef vector<pl> vpl;
 typedef vector<vi> vvi;
 typedef vector<vl> vvl;
 mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+ 
+ 
+void solve() {
 
-void solve()
-{
-   
+ }
+ 
+ 
+int main() {
+ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+int t = 1;
+cin >> t;
+while(t--) {
+solve();
 }
-
-int main()
-{
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
-
-    solve();
-
-    return 0;
+return 0;
 }
